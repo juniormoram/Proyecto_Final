e7889feb3c01cd4a4b04ad3487d489fe4372daa1
Dereck <dereckaguilar@gmail.com>
2022-04-11 12:26:11 -0600

    add navigation to create invoices, create invoices page

--- app/src/main/java/com/example/proyecto_final/InicioActivity.kt
+++ app/src/main/java/com/example/proyecto_final/InicioActivity.kt
@@ -1,14 +1,18 @@
 package com.example.proyecto_final
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_inicio.*
 
 class InicioActivity:AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
-
+        btn_Crear_Factura.setOnClickListener{
+            startActivity(Intent(this,InicioActivity::class.java))
+        }
     }
 
 }
