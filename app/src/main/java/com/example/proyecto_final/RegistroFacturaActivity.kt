d3c36d6c2a72889c4db60bbfdac774eac9ad60fd
Dereck <dereckaguilar@gmail.com>
2022-04-19 14:25:55 -0600

    edit create invoice view

--- app/src/main/java/com/example/proyecto_final/RegistroFacturaActivity.kt
+++ app/src/main/java/com/example/proyecto_final/RegistroFacturaActivity.kt
@@ -1,32 +1,23 @@
 package com.example.proyecto_final
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_crear_factura.*
 
 
 class RegistroFacturaActivity : AppCompatActivity() {
-
-    private val COUNTRIES = arrayOf(
-        "Belgium", "France", "Italy", "Germany", "Spain"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_factura)
 
-        val selectEmisor: Spinner = findViewById(R.id.emisorSelection)
-        val selectReceptor: Spinner = findViewById(R.id.receptorSelection)
-        //Agrega datos de forma dinamica al select, 
-        //esto hay que cambiarlo una vez se obtengan los datos correspondientes del API
-        val spinnerArrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(
-            this,
-            android.R.layout.simple_spinner_item,
-            COUNTRIES
-        )
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
-        selectEmisor.adapter = spinnerArrayAdapter
-        selectReceptor.adapter = spinnerArrayAdapter
+        emisorBtn.setOnClickListener{
+            startActivity(Intent(this,RegistroFacturaActivity::class.java))
+        }
+        receptorBtn.setOnClickListener{
+            startActivity(Intent(this,RegistroFacturaActivity::class.java))
+        }
+
     }
 }
