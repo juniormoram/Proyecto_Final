436fc9a5a74be098898c7c60d8fc4299536eed21
mmora28 <melangel28@gmail.com>
2022-04-19 15:26:16 -0600

    Seleccionar emisor y receptor

---
+++ app/src/main/java/com/example/proyecto_final/adapter_emisor.kt
@@ -0,0 +1,60 @@
+package com.example.proyecto_final
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class adapter_emisor : RecyclerView.Adapter<adapter_emisor.ViewHolder>()  {
+    var emisores:MutableList<RegistroFacturaActivity.PERSONA> = ArrayList()
+
+    lateinit var context: Context
+
+    fun adapter_emisor(emisores: MutableList<RegistroFacturaActivity.PERSONA>, context: Context){
+        this.emisores = emisores
+        this.context = context
+
+    }
+    override fun onBindViewHolder(holder: adapter_emisor.ViewHolder, position: Int) {
+        val item = emisores.get(position)
+        holder.bind(item, context)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): adapter_emisor.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return adapter_emisor.ViewHolder(layoutInflater.inflate(R.layout.item_layout_emisor, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return emisores.size
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val nombre = view.findViewById(R.id.nombre) as TextView
+        val numero = view.findViewById(R.id.numero) as TextView
+        val correo = view.findViewById(R.id.correo) as TextView
+        val botonselecciona = view.findViewById(R.id.btnselecciona) as Button
+
+
+        fun bind(emisor: RegistroFacturaActivity.PERSONA, context: Context){
+            nombre.text = emisor.NOMBRE1
+            numero.text = emisor.NUMERO1
+            correo.text= emisor.CORREOELECTRONICO1
+
+
+        }
+        fun ImageView.loadUrl(url: String) {
+            Picasso.with(context).load(url).into(this)
+        }
+    }
+
+
+
+
+}
